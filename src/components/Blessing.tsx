f14050ad073474955150e670790deebcfa07afc7
GAURAV07C <gaurav989267@gmail.com>
2024-10-31 15:14:02 +0530

    commites

--- src/components/Blessing.tsx
+++ src/components/Blessing.tsx
@@ -18,7 +18,7 @@ const Blessing = () => {
           />
         </div>
 
-        <div className="     lg:order-firsts mb-16 lg:w-[864px] lg:h-[722px] lg:-mt-[730px] lg:-ml-[150px]">
+        <div className="     lg:order-firsts mb-16 lg:w-[864px] lg:h-[722px] lg:-mt-[730px] lg:-ml-[120px]">
           <Image
             src="/mandapa.png"
             alt="mandap"
